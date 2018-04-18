6336c9869366846a797962a9501c2ef0a903ffaf
Yang Liu <foamliu@yeah.net>
2018-04-18 10:53:16 +0800

    reduce sampling rate

--- capture_video.py
+++ capture_video.py
@@ -17,16 +17,20 @@ time.sleep(0.1)
 face_cascade = cv2.CascadeClassifier('/home/pi/opencv-3.3.0/data/haarcascades/haarcascade_frontalface_alt.xml')
 
 start = time.time()
+frame_id = 0
 # capture frames from the camera
 for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
     # grab the raw NumPy array representing the image, then initialize the timestamp
     # and occupied/unoccupied text
     image = frame.array
     image = cv2.flip(image, 0)
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(gray, 1.1, 5)
-    for (x, y, w, h) in faces:
-        image = cv2.rectangle(image, (x, y), (x + w, y + h), (255, 0, 0), 2)
+
+    if frame_id % 20 == 0:
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        faces = face_cascade.detectMultiScale(gray, 1.1, 5)
+    if faces:
+        for (x, y, w, h) in faces:
+            image = cv2.rectangle(image, (x, y), (x + w, y + h), (255, 0, 0), 2)
 
     end = time.time()
     seconds = end - start
@@ -36,7 +40,9 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
     # show the frame
     cv2.imshow("Frame", image)
     key = cv2.waitKey(1) & 0xFF
+
     start = time.time()
+    frame_id += 1
 
     # clear the stream in preparation for the next frame
     rawCapture.truncate(0)
