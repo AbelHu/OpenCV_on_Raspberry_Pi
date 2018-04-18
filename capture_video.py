e93e9c227c0f826fb08114584b5a582ead1935cb
Yang Liu <foamliu@yeah.net>
2018-04-18 10:49:34 +0800

    show fps

--- capture_video.py
+++ capture_video.py
@@ -3,6 +3,7 @@ from picamera.array import PiRGBArray
 from picamera import PiCamera
 import time
 import cv2
+from common import draw_str
 
 # initialize the camera and grab a reference to the raw camera capture
 camera = PiCamera()
@@ -15,6 +16,7 @@ time.sleep(0.1)
 
 face_cascade = cv2.CascadeClassifier('/home/pi/opencv-3.3.0/data/haarcascades/haarcascade_frontalface_alt.xml')
 
+start = time.time()
 # capture frames from the camera
 for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
     # grab the raw NumPy array representing the image, then initialize the timestamp
@@ -23,12 +25,18 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
     image = cv2.flip(image, 0)
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, 1.1, 5)
-    for (x,y,w,h) in faces:
-        image = cv2.rectangle(image,(x,y),(x+w,y+h),(255,0,0),2)
-        
+    for (x, y, w, h) in faces:
+        image = cv2.rectangle(image, (x, y), (x + w, y + h), (255, 0, 0), 2)
+
+    end = time.time()
+    seconds = end - start
+    fps = 1.0 / seconds
+    draw_str(image, (20, 20), 'fps: %d' % (fps))
+
     # show the frame
     cv2.imshow("Frame", image)
     key = cv2.waitKey(1) & 0xFF
+    start = time.time()
 
     # clear the stream in preparation for the next frame
     rawCapture.truncate(0)
