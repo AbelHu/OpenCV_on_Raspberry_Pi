2e5ab8e9c9250bea1cf80eb13fef5404f559355c
Yang Liu <foamliu@yeah.net>
2018-04-23 16:04:57 +0800

    send message to kafka

--- capture_video.py
+++ capture_video.py
@@ -4,12 +4,16 @@ from picamera import PiCamera
 import time
 import cv2
 from common import draw_str
+from kafka import KafkaProducer
+from time import gmtime, strftime
 
 # initialize the camera and grab a reference to the raw camera capture
 camera = PiCamera()
 camera.resolution = (640, 480)
 camera.framerate = 32
 rawCapture = PiRGBArray(camera, size=(640, 480))
+producer = KafkaProducer(bootstrap_servers=['10.156.91.65:9092'], api_version=(0, 10))
+num_faces = 0
 
 # allow the camera to warmup
 time.sleep(0.1)
@@ -27,11 +31,21 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
 
     #if frame_id % 30 == 0:
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    faces = face_cascade.detectMultiScale(gray, 1.2, 5)
+    cur_num_faces = len(faces)
+
     if faces is not None and len(faces) > 0:
         for (x, y, w, h) in faces:
             image = cv2.rectangle(image, (x, y), (x + w, y + h), (255, 0, 0), 2)
 
+    if cur_num_faces > num_faces:
+        message = "[{}] Detected {} face(s).".format(strftime("%Y-%m-%d %H:%M:%S", gmtime()), cur_num_faces)
+        b_msg = bytearray()
+        b_msg.extend(map(ord, message))
+        producer.send('test', b_msg)
+        producer.flush()
+
+    num_faces = cur_num_faces
     end = time.time()
     seconds = end - start
     fps = 1.0 / seconds
--- capture_video_pc.py
+++ capture_video_pc.py
@@ -3,6 +3,7 @@ import time
 import cv2 as cv
 from common import draw_str
 from kafka import KafkaProducer
+from time import gmtime, strftime
 
 # initialize the camera and grab a reference to the raw camera capture
 cap = cv.VideoCapture(0)
@@ -19,16 +20,20 @@ while True:
     ret, frame = cap.read()
 
     gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    faces = face_cascade.detectMultiScale(gray, 1.2, 5)
     cur_num_faces = len(faces)
     if faces is not None and cur_num_faces > 0:
         for (x, y, w, h) in faces:
             image = cv.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
 
-    if cur_num_faces != num_faces:
-        producer.send('test', b"Hello from python")
+    if cur_num_faces > num_faces:
+        message = "[{}] Detected {} face(s).".format(strftime("%Y-%m-%d %H:%M:%S", gmtime()), cur_num_faces)
+        b_msg = bytearray()
+        b_msg.extend(map(ord, message))
+        producer.send('test', b_msg)
         producer.flush()
-        num_faces = cur_num_faces
+
+    num_faces = cur_num_faces
 
     end = time.time()
     seconds = end - start
