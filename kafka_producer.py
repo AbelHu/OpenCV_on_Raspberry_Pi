f15fcae4e25af10614a3f2bffc66e48ace6b9728
Ubuntu <azurestackuser@hdpmaster0.nuhddguwgaae1okedojd55rvzf.bx.internal.cloudapp.net>
2018-04-23 07:24:06 +0000

    no need

--- kafka_producer.py
+++ kafka_producer.py
@@ -1,7 +1,7 @@
 from kafka import KafkaProducer
 from kafka.errors import KafkaError
 
-producer = KafkaProducer(bootstrap_servers=['10.156.91.65:9092'], api_version=(0, 10))
+producer = KafkaProducer(bootstrap_servers=['10.156.91.65:9092'])
 message = "Hello from python"
 b_msg = bytearray()
 b_msg.extend(map(ord, message))
