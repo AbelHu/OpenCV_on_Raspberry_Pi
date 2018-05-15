c7215d2f7d146fdfecc746c37030be5bf2b776d3
Yang Liu <foamliu@yeah.net>
2018-05-15 16:06:14 +0800

    update host

--- kafka_producer.py
+++ kafka_producer.py
@@ -1,7 +1,7 @@
 from kafka import KafkaProducer
 from kafka.errors import KafkaError
 
-producer = KafkaProducer(bootstrap_servers=['demomaster0.shanghai.cloudapp.azurestack.corp.microsoft.com:9092'])
+producer = KafkaProducer(bootstrap_servers=['dnshdpmaster0.shanghai.cloudapp.azurestack.corp.microsoft.com:9092'])
 message = "Hello from python"
 b_msg = bytearray()
 b_msg.extend(map(ord, message))
